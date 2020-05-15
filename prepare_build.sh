9dfcfc2766552df92a10d13ddc59d9a6dc60b070
ampersand8 <cicd@concourse.ci>
2020-05-15 09:18:46 +0200

    bug: Update Spring Repo Address
    repo.spring.io does not allow access over http

--- prepare_build.sh
+++ prepare_build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Given repo in credhub project forbids http access
+sed -i 's/http\:/https\:/g' credhub/build.gradle
 cp truststore_preparation.sh credhub/
 cp fetch-version.sh credhub/
 cd credhub
